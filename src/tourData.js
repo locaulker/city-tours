08fd0d9d589460f8adceacea5ff3ee11e1a84efb
Lawrence <Caulker>
2020-04-17 23:19:22 -0400

    completed initial setup

---
+++ src/tourData.js
@@ -0,0 +1,34 @@
+export const tourData = [
+  {
+    id: 1,
+    city: "new york",
+    img: "./img/newyork.jpeg",
+    name: "new york bridge tour",
+    info:
+      "Few people know that the eyebrows can be combed not only to give them the right shape, but also in order to enhance their growth and increase the density."
+  },
+  {
+    id: 2,
+    city: "paris",
+    img: "./img/paris.jpeg",
+    name: "paris lights tour",
+    info:
+      "Very important characteristic and great value of any vegetables is their freshness. Fresh vegetables is source of vitamins and minerals which are required for human body for normal passing of all processes of life activity."
+  },
+  {
+    id: 3,
+    city: "london",
+    img: "./img/london.jpeg",
+    name: "london royal palace tour",
+    info:
+      "In 1979, UNESCO contributed the Old City of Dubrovnik to the World Heritage List, including a significant part of the ancient city walls."
+  },
+  {
+    id: 4,
+    city: "tokyo",
+    img: "./img/tokyo.jpeg",
+    name: "tokyo sushi tour",
+    info:
+      "Cars janitors are subdivided into 3 look: these are frame, frameless and hybrid. Frame - ordinary type of brushes of windshield where the clearing tape to fasten through several yokes."
+  }
+];
